321637900bdf3239d6350f6a27edd7b5d7e345e2
alanjames00 <alanjames6282@gmail.com>
2023-11-18 18:20:03 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Layout from "./Layout";
+import HomePage from "@/components/HomePage";
+
+function App() {
+  return (
+    <Layout>
+      <HomePage />
+    </Layout>
+  );
+}
+
+export default App;
